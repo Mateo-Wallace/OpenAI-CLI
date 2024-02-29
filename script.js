a658120aa28a0e9dd3d2177ce61db84d7ceecc9a
Mateo Wallace <mateo.t.wallace@gmail.com>
2024-02-29 13:15:39 -0500

    Prompt engineered the user input and formatted the response

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // dependencies
-const { OpenAI } = require("langchain/llms/openai");
+const { OpenAI } = require("@langchain/openai");
+const { PromptTemplate } = require("@langchain/core/prompts");
+const { StructuredOutputParser } = require("langchain/output_parsers");
 const inquirer = require("inquirer");
 require("dotenv").config();
 
@@ -13,8 +15,26 @@ const model = new OpenAI({
 // Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
 const promptFunc = async (input) => {
   try {
-    const res = await model.call(input);
-    console.log(res);
+    // With a `StructuredOutputParser` we can define a schema for the output.
+    const parser = StructuredOutputParser.fromNamesAndDescriptions({
+      code: "Javascript code that answers the user's question",
+      explanation: "detailed explanation of the example code provided",
+    });
+    const formatInstructions = parser.getFormatInstructions();
+
+    // Instantiation of a new object called "prompt" using the "PromptTemplate" class
+    const prompt = new PromptTemplate({
+      template:
+        "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{format_instructions}\n{question}",
+      inputVariables: ["question"],
+      partialVariables: { format_instructions: formatInstructions },
+    });
+    const promptInput = await prompt.format({
+      question: input,
+    });
+
+    const res = await model.invoke(promptInput);
+    console.log(await parser.parse(res));
   } catch (err) {
     console.error(err);
   }
